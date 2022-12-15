e4c3d67f9a5fcf95ad66be27255fff10cea95ee1
marcella <marcella.camara@sagatechbrasil.com.br>
2022-12-15 04:18:49 -0300

    integrando com a pokeAPI

---
+++ pokedex/assets/js/poke-api.js
@@ -0,0 +1,18 @@
+//saída deste arquivo: um objeto em que contém as funções de manipulação da poke API
+
+const pokeApi = {};
+
+pokeApi.getPokemonDetail = (pokemon) => {
+  return fetch(pokemon.url).then((response) => response.json());
+};
+
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+  return fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonsDetails) => pokemonsDetails);
+};
